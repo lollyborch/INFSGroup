9d054d4a2573faca3114bcb0a8e8b349fd5490a6
lollyborch <lilly.borchardt@gmail.com>
2016-05-29 18:46:40 +1000

    about modal
    trying to get modal close to work when logged in.

--- js/modal.js
+++ js/modal.js
@@ -1,23 +1,32 @@
-var aboutModal = document.getElementById("aboutModal");
-var aboutButton = document.getElementById("aboutButton");
-var loginModal = document.getElementById("loginModal");
-var loginButton = document.getElementById("loginButton");
-var closeAbout = document.getElementById("closeAbout");
-var closeLogin = document.getElementById("closeLogin");
+window.onload = function(){
+    var aboutModal = document.getElementById("aboutModal");
+    var aboutButton = document.getElementById("aboutButton");
+
+    var loginModal = document.getElementById("loginModal");
+    var loginButton = document.getElementById("loginButton");
+
+    var closeAbout = document.getElementById("closeAbout");
+    var closeLogin = document.getElementById("closeLogin");
 
 
-aboutButton.onclick = function() {
-  aboutModal.style.display = "block";
+    aboutButton.onclick = function() {
+      aboutModal.style.display = "block";
+    }
+
+    loginButton.onclick = function() {
+      loginModal.style.display = "block";
+    }
+
+    closeAbout.onclick = function() {
+      aboutModal.style.display = "none";
+    }
+
+    closeLogin.onclick = function() {
+      loginModal.style.display = "none";
+    } 
 }
 
-loginButton.onclick = function() {
-  loginModal.style.display = "block";
-}
 
-closeAbout.onclick = function() {
-  aboutModal.style.display = "none";
-}
-
-closeLogin.onclick = function() {
-  loginModal.style.display = "none";
-}
+$(document).on('click','#closeAbout',function() {
+    alert("Hello World");
+}); 
